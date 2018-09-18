dd015790fe5d1e8bf8ec30a5323488e45f11d237
Tanya Lipnitskaya <fenya-ne@yandex.ru>
2018-09-18 23:08:15 +0700

    local storage is created

--- New-to-do/index.js
+++ New-to-do/index.js
@@ -1,36 +1,67 @@
-const currentDateElement=document.querySelector(".Date");
-currentDateElement.innerHTML=new Date().toLocaleDateString();
+const superStorageKey = "key";
+let taskArray = [];
 
-const listOfCurrentTasks=document.querySelector(".listOfCurrentTasks");
-const createNewTask=(taskTitle)=>{
-    const listItem=document.createElement("li");
-    listItem.innerHTML=taskTitle;
+const currentDateElement = document.querySelector(".Date");
+currentDateElement.innerHTML = new Date().toLocaleDateString();
+
+const listOfCurrentTasks = document.querySelector(".listOfCurrentTasks");
+const createNewTask = (taskTitle) => {
+    taskArray.push(taskTitle);
+    localStorage.setItem(superStorageKey, JSON.stringify(taskArray));
+    const listItem = document.createElement("li");
+    listItem.innerHTML = taskTitle.taskName;
     listOfCurrentTasks.appendChild(listItem);
+}
+
+const localStorageTaskArray = localStorage.getItem(superStorageKey)
+if (localStorageTaskArray) {
+    const tempVar = JSON.parse(localStorageTaskArray);
+   
+    for (let arrayElement of tempVar) {
+        createNewTask(arrayElement)
+
+    }
 
 }
 
 
-const addTask=document.querySelector(".addTask");
-const modalWindow=document.querySelector('.Modal')
-const addTaskOnClick= ()=>{
+const addTask = document.querySelector(".addTask");
+const modalWindow = document.querySelector('.Modal')
+const addTaskOnClick = () => {
     modalWindow.classList.toggle("Modal-disabled");
-     
 }
 addTask.addEventListener("click", addTaskOnClick);
 
-const currentForm=document.querySelector('form[name="Form"]');
-const currentFormOnSubmit=(event)=> {
+const currentForm = document.querySelector('form[name="Form"]');
+const currentFormOnSubmit = (event) => {
     event.preventDefault()
-    const formElement=event.target.elements;
-    const taskName=formElement.taskName.value;
-    createNewTask(taskName);
+    const formElement = event.target.elements;
+    const taskName = formElement.taskName.value;
+    const taskDescription = formElement.taskDescription.value;
+    const selectPriority = formElement.selectPriority.value;
+    const plannedDate = formElement.plannedDate.value;
+    const actualDate = formElement.actualDate.value;
+
+    const taskItem = {
+        taskName,
+        taskDescription,
+        selectPriority,
+        plannedDate,
+        actualDate
+    };
+
+    createNewTask(taskItem);
+
     // modalWindow.classList.add("Modal-disabled");
     currentForm.reset();
 
+
 };
+
+
 currentForm.addEventListener("submit", currentFormOnSubmit);
 
-const currentFormOnReset=()=> {
-       modalWindow.classList.add("Modal-disabled");
+const currentFormOnReset = () => {
+    modalWindow.classList.add("Modal-disabled");
 }
 currentForm.addEventListener("reset", currentFormOnReset);
